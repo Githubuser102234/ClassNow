4add6a416019f543bf74ec3f3586c7978da9f875
Githubuser102234 <119143251+Githubuser102234@users.noreply.github.com>
2025-09-16 03:33:21 -0400

    Delete vercel.js

--- vercel.js
+++
@@ -1,9 +0,0 @@
-{
-"rewrites": [
-{ "source": "/classroom/:id", "destination": "/api/classroom/[id]" }
-],
-"builds": [
-{ "src": "api/.js", "use": "@vercel/node" },
-{ "src": "api/classroom/.js", "use": "@vercel/node" }
-]
-}
